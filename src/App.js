82413fe88522e799d78b0be7bf28e1e8bf06a352
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2020-08-20 16:49:11 +0545

    Function Component - Counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
 
 function App() {
-  return <Counter initialCount={0}/>
+    return (
+        <>
+            Class Counter
+            <Counter initialCount={0}/>
+            Counter Hooks
+            <CounterHooks initialCount={0}/>
+        </>
+    )
 }
 
 export default App;
